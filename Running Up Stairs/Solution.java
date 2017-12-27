71a94cb6072b2999b6ce5a7a58d5d0096579a1e8
Omar Dahmen <omardahmen@gmail.com>
2017-12-27 13:01:03 +0100

    adding Running Up Stairs source code

--- Running Up Stairs/Solution.java
+++ Running Up Stairs/Solution.java
@@ -1,1 +1,21 @@
-// Score : ..%
+// Score : 50%
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Please name your class Main
+class Main {
+  public static void main (String[] args) throws java.lang.Exception {
+    Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int n;
+	  final double GR = (1 + Math.sqrt(5)) / 2;
+    final double NGR = -GR + 1;
+	    
+		for(int i=0;i<t;i++){
+      n = in.nextInt()+2;
+      System.out.println( Math.round(1 / Math.sqrt(5) * (Math.pow(GR, n) - Math.pow(NGR, n)))); 
+		}
+	}
+}
